8467ff09cb863aad8b8af00bb642319e68454f0e
Light <di759026@gmail.com>
2021-10-18 19:13:11 +0300

    header

--- layouts/header.vue
+++ layouts/header.vue
@@ -6,18 +6,29 @@
           <menu-btn></menu-btn>
           <ul class="header__contacts">
             <li class="header__restaurants header__contacts_item">
-              <span @click="isRestaurantsChecking">Рестораны <fa icon="caret-down"></fa></span>
+              <span @click="isRestaurantsChecking"
+                >Рестораны <fa :icon="caretRest"></fa
+              ></span>
               <div class="header__restaurants_menu" v-if="isRestaurants">
                 <h3 class="header__restaurants_title">Наши Рестораны</h3>
                 <ul class="header__restaurants_list">
                   <li class="header__restaurants_rest">Улица Козака, 3</li>
                   <li class="header__restaurants_rest">Проспект Бубки, 9А</li>
-                  <li class="header__restaurants_rest">Бульвар Каштанский, 12</li>
+                  <li class="header__restaurants_rest">
+                    Бульвар Каштанский, 12
+                  </li>
                 </ul>
               </div>
             </li>
             <li class="header__contacts header__contacts_item">
-              <span>Контакты <fa icon="caret-down"></fa></span>
+              <span @click="isContactsChecking">Контакты <fa :icon="caretCont"></fa></span>
+              <div class="header__contacts_menu" v-if="isContacts">
+                <h3 class="header__contacts_title">Наши Контакты</h3>
+                <ul class="header__contacts_list">
+                  <li class="header__contacts_contact"><a href="tel:+38(066)9521840">(066) 952-18-40</a></li>
+                  <li class="header__contacts_contact">di759026@gmail.com</li>
+                </ul>
+              </div>
             </li>
             <li class="header__getItem header__contacts_item">
               <span>Заберу сам</span>
@@ -52,12 +63,36 @@ export default {
   name: "headerComp",
   data() {
     return {
-      isRestaurants: false
-    }
+      isRestaurants: false,
+      isContacts: false,
+      caretRest: "caret-down",
+      caretCont: "caret-down"
+    };
   },
   methods: {
     isRestaurantsChecking() {
-      this.isRestaurants = this.isRestaurants ? false : true
+      this.isRestaurants = this.isRestaurants ? false : true;
+      if (this.isContacts) {
+        this.isContacts = false,
+        this.caretCont = 'caret-down'
+      }
+      if (this.isRestaurants) {
+        this.caretRest = "caret-up";
+      } else {
+        this.caretRest = "caret-down";
+      }
+    },
+    isContactsChecking() {
+      this.isContacts = this.isContacts ? false : true;
+      if (this.isRestaurants) {
+        this.isRestaurants = false,
+        this.caretRest = 'caret-down'
+      }
+      if (this.isContacts) {
+        this.caretCont = "caret-up";
+      } else {
+        this.caretCont = "caret-down";
+      }
     }
   },
   components: {
